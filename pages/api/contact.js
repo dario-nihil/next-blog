34066a8230e5d211846038f7c7ffb2f3765347dc
dario-nihil <dario.nihil@gmail.com>
2023-01-10 16:12:47 +0100

    created api route for contact

---
+++ pages/api/contact.js
@@ -0,0 +1,25 @@
+const handler = (req, res) => {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      return res.status(422).json({ message: "Invalid input." });
+    }
+
+    const newMessage = { email, name, message };
+    console.log(newMessage);
+
+    return res
+      .status(201)
+      .json({ message: "Successfully stored message!", message: newMessage });
+  }
+};
+
+export default handler;
