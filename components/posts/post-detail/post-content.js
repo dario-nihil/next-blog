14d4800e39f99c371e0255473f5c0203237119e2
dario-nihil <dario.nihil@gmail.com>
2023-01-10 11:23:25 +0100

    installed react-markdown package

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,8 @@
-import styles from "./post-content.module.css";
+import reactMarkdown from "react-markdown";
+
 import PostHeader from "./post-header";
+import styles from "./post-content.module.css";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 const DUMMY_POSTS = {
   slug: "getting-started-with-nextjs",
@@ -15,7 +18,7 @@ const PostContent = () => {
   return (
     <article className={styles.content}>
       <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
-      {DUMMY_POSTS.content}
+      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
     </article>
   );
 };
