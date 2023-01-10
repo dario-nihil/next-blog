8b3b6c723780bc71d711bf0f0a297f76cd297232
dario-nihil <dario.nihil@gmail.com>
2023-01-10 22:17:29 +0100

    updated success message notification

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -75,7 +75,7 @@ const ContactForm = () => {
     notification = {
       status: "success",
       title: "Success!",
-      message: "Message sent successfully!!!!",
+      message: "Message sent successfully!!!!!!!!",
     };
   }
 
