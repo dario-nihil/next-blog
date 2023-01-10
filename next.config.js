ec5420babdcf1eb7b6b7d8ff9e4d8bf40492166f
dario-nihil <dario.nihil@gmail.com>
2023-01-10 21:16:49 +0100

    included env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfigDev = {
+  reactStrictMode: true,
+  env: {
+    mongodb_username: "dario",
+    mongodb_password: "BNFJu2qjdTQXsTv4",
+    mongodb_clustername: "cluster0",
+    mongodb_database: "my-site-dev",
+  },
+};
+
+const nextConfigProd = {
+  reactStrictMode: true,
+  env: {
+    mongodb_username: "dario",
+    mongodb_password: "BNFJu2qjdTQXsTv4",
+    mongodb_clustername: "cluster0",
+    mongodb_database: "my-site",
+  },
+};
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return nextConfigDev;
+  }
+
+  return nextConfigProd;
+};
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -18,10 +18,10 @@ const handler = async (req, res) => {
     const newMessage = { email, name, message };
     let client;
 
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.clustername}.ovnq1fr.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        "mongodb+srv://dario:BNFJu2qjdTQXsTv4@cluster0.ovnq1fr.mongodb.net/my-site?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       return res
         .status(500)
