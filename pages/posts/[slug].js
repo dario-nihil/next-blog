7e556c88d301e46e8e4f9377de2ad2e13fcfeb15
dario-nihil <dario.nihil@gmail.com>
2023-01-10 20:26:41 +0100

    added Head component to the pages

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,18 @@
+import Head from "next/head";
+
 import PostContent from "../../components/posts/post-detail/post-content";
 import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
 const PostDetailPage = ({ post }) => {
-  return <PostContent post={post} />;
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.excerpt} />
+      </Head>
+      <PostContent post={post} />
+    </>
+  );
 };
 
 export const getStaticProps = (context) => {
