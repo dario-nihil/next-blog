74df350951be3e91cd035198dd7b25b0a03d99f7
dario-nihil <dario.nihil@gmail.com>
2023-01-10 16:55:51 +0100

    installed mongodb package and implemented storing message into mongodb atlas solution

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -14,7 +16,29 @@ const handler = (req, res) => {
     }
 
     const newMessage = { email, name, message };
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://dario:BNFJu2qjdTQXsTv4@cluster0.ovnq1fr.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Could not connect to database." });
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      return res.status(500).json({ message: "Storing message failed!" });
+    }
+
+    client.close();
 
     return res
       .status(201)
